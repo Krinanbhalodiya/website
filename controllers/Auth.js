9957f60414f3b81807b3edb73511a4bf9cc429c0
Krina Bhalodiya <166689919+Krinanbhalodiya@users.noreply.github.com>
2025-02-05 12:22:48 +0530

    Update Auth.js

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,68 +1,89 @@
 import UserModel from "../models/user.js";
-import jwt from "jsonwebtoken";
-import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs';
+import crypto from 'crypto';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
 const register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
-        
-        const existUser = await UserModel.findOne({ email });    
+
+        const existUser = await UserModel.findOne({ email });
         if (existUser) {
-            return res.status(401).json({ success:false,message: "User already exist" });
+            return res.status(401).json({ success: false, message: "User already Exist" });
         }
-        const hasepassword = await bcryptjs.hash(password, 10);
+        const hasepassword = await bcryptjs.hashSync(password, 10);
         const newUser = new UserModel({
-            name,
-            email,
-            password: hasepassword
+            name, email, password: hasepassword
         });
+
         await newUser.save();
 
-        res.status(200).json({Message:"users register successfully",newUser});
+        res.status(200).json({ message: "user register successfully", newUser });
     } catch (error) {
-        res.status(500).json({success:false, message: "Something went wrong"
-    })
-    console.log(error);
+        res.status(500).json({ success: false, message: "internal server error" });
+        console.log(error);
     }
+};
 
-}
-
-
-const login = async (req, res) => {
+const Login=async(req,res)=>{
     try {
-        const { email, password } = req.body;
-        const user = await UserModel.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ success:false,message: "User not found" });
-        }
+          const {email,password}=req.body
 
-        const ispasswordValid = await bcryptjs.compare(password, user.password);
-        if (!ispasswordValid) {
-            return res.status(404).json({ success:false,message: "Invalid credentials" });
-        }
-        const token = jwt.sign({userId:user._id}, process.env.JWT_SECRETE)
+          const user=await UserModel.findOne({email})
 
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: false,
-            maxAge: 3600000
-        });
-        res.status(200).json({ success:true, message: "User login successfully", user,token });
+          if (!user) {
+              return res.status(404).json({success:false,message:"Invalid credentials"})
+          }
+
+          const ispassaowrdValid= await bcryptjs.compare(password,user.password)
+          if (!ispassaowrdValid) {
+            return res.status(404).json({success:false,message:"Invalid credentials"})
+            
+          }
+               const token= jwt.sign({userId:user._id},process.env.JWT_SECRETE)
+
+                res.cookie('token',token,{
+                    httpOnly: true,
+                    secure: false,
+                    maxAge: 3600000,
+                    
+                })
+              res.status(200).json({success:true,message:"Login successfully",user,token})
 
     } catch (error) {
-        res.status(500).json({success:false, message: "Something went wrong"    })      
-        console.log(error);
-    }   
+        res.status(500).json({success:false,message:"interanl server ereo"})
+        console.log(error)
+    }
 }
-   const logout = async (req, res) => {
+  const Logout=async(req,res)=>{
     try {
-        res.clearCookie('token');
-        res.status(200).json({ success:true, message: "User logout successfully" });
+        res.clearCookie('token')
+        res.status(200).json({message:"user Logout successfully"})
     } catch (error) {
-        res.status(500).json({success:false, message: "Something went wrong"    })      
-        console.log(error);
-    }     
-}
+        res.status(500).json({success:false,message:"interanl server ereo"})
+        console.log(error)
+    }
+  }
+     const CheckUser=async(req,res)=>{
+            try {
+                const user=req.user
+                if (!user) {
+                    res.status(404).json({message:'User not found'})
+                }
+                res.status(200).json(user)
 
-
-
-export { register, login , logout};
+                
+            } catch (error) {
+                res.status(500).json({message:"internal server error"})
+                console.log(error)
+                
+            }
+     }
+     
+export {register,Login,Logout}
+    
