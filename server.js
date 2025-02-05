46b962271ae0dd8c12992c8fcc5fa8d3e8fc825b
Krina Bhalodiya <166689919+Krinanbhalodiya@users.noreply.github.com>
2025-02-05 12:27:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import DBCon from './utils/db.js';
-import AuthRoutes from './routes/Auth.js';
-import AdminRoutes from './routes/AdminRoutes.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieparser from 'cookie-parser'
+import DbCon from './utlis/db.js'
+import AuthRoutes from './routes/Auth.js'
+import AdminRoutes from './routes/AdminRoutes.js'
+dotenv.config()
+const PORT=process.env.PORT || 3000
+const app=express()
 
-dotenv.config();
-const PORT=process.env.PORT || 3000;
-const app = express();
+// mongo db 
+DbCon()
+app.use(express.json())
+app.use(cookieparser())
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173'  
+}));
 
-//mongo db
-DBCon();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors());
+app.use('/api/auth',AuthRoutes)
+app.use('/api/admin',AdminRoutes)
 
-app.use('/api/auth', AuthRoutes);
-app.use('/api/admin', AdminRoutes);
-
-
-app.get('/', (req, res) => {
-  res.send('world!');
+app.get('/',(req,res)=>{
+    res.send('test')
 })
 
-app.listen(PORT, () => { 
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
